3eacbe483fdc8c48cba53474983f5be810712e85
lerman <lerman01@gmail.com>
2019-08-16 15:41:40 +0300

    - omptimizing fetch

--- src/main/java/spotlightextractor/Worker.java
+++ src/main/java/spotlightextractor/Worker.java
@@ -2,8 +2,10 @@ package spotlightextractor;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.neovisionaries.i18n.LocaleCode;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.LocaleUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -17,24 +19,21 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 
 public class Worker implements Runnable {
 
     private static final Logger logger = LogManager.getLogger(Worker.class);
 
-    private static final String URL = "https://arc.msn.com/v3/Delivery/Cache?pid=%s&ctry=%s&lc=en&fmt=json&lo=%s";
+    private static final String URL = "https://arc.msn.com/v3/Delivery/Cache?pid=%s&ctry=%s&lc=%s&fmt=json&lo=%s";
     private static final List<String> PID_LIST = Arrays.asList("209567", "279978", "209562");
-    private static final List<String> COUNTRIES_LIST = Arrays.asList("en", "de", "us");
     private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36";
     private static final Random randomGenerator = new Random();
     private static final String IMAGES_FOLDER = "images";
     private static final String JPG = "JPG";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final LocaleCode[] LOCALE_LIST = LocaleCode.values();
 
     private static List<File> imagesList;
     private CloseableHttpClient httpclient;
@@ -60,35 +59,45 @@ public class Worker implements Runnable {
         return imagesList;
     }
 
+    private static LocaleCode getRandomLocale() {
+        LocaleCode localeCode;
+        do {
+            localeCode = LOCALE_LIST[randomGenerator.nextInt(LOCALE_LIST.length)];
+        } while (localeCode.getCountry() == null || localeCode.getCountry().getName().equals("Undefined"));
+        return localeCode;
+    }
+
     public void run() {
         fetchData();
     }
 
     private void fetchData() {
-        for (String country : COUNTRIES_LIST) {
-            try {
-                ImageData imageData = fetchImageData(country);
+        try {
+            ImageData imageData = fetchImageData();
+            if (imageData != null) {
                 byte[] imageBytes = fetchImage(imageData);
                 if (!saveImageIfNotExists(imageData, imageBytes)) {
                     logger.info(String.format("New image added : %s", imageData));
                 } else {
                     logger.debug(String.format("File already exists : %s", imageData));
                 }
-            } catch (Exception e) {
-                logger.error("Error while get image", e);
             }
+        } catch (Exception e) {
+            logger.error("Error while get image", e);
         }
     }
 
-    private ImageData fetchImageData(String country) throws Exception {
+    private ImageData fetchImageData() throws Exception {
         CloseableHttpResponse response = null;
         String imageUrl = null;
         String imageDescription = null;
         String imageId = null;
         HttpEntity entity1;
         String body = null;
+        boolean ignoreRequest = false;
         try {
-            HttpGet httpGet = new HttpGet(String.format(URL, PID_LIST.get(randomGenerator.nextInt(PID_LIST.size())), country, randomGenerator.nextInt(900000) + 100000));
+            LocaleCode randomLocale = getRandomLocale();
+            HttpGet httpGet = new HttpGet(String.format(URL, PID_LIST.get(randomGenerator.nextInt(PID_LIST.size())), randomLocale.getCountry().toString().toLowerCase(), randomLocale.getLanguage().toString().toLowerCase(), randomGenerator.nextInt(900000) + 100000));
             httpGet.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache");
             httpGet.setHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate");
             response = httpclient.execute(httpGet);
@@ -101,21 +110,30 @@ public class Worker implements Runnable {
             JsonNode batchrsp = OBJECT_MAPPER.readTree(body).get("batchrsp");
             JsonNode errors = batchrsp.get("errors");
             if (errors != null) {
-                throw new Exception(String.format("Error in response: %s", body));
+                int errorCode = errors.get(0).get("code").asInt();
+                if (errorCode == 2040 || errorCode == 2000) {
+                    ignoreRequest = true;
+                } else {
+                    throw new Exception(String.format("Error in response: %s", body));
+                }
             }
-            item = OBJECT_MAPPER.readTree(body).get("batchrsp").get("items").get(0).get("item").asText();
-            JsonNode jsonNode = OBJECT_MAPPER.readTree(item);
-            imageUrl = jsonNode.get("ad").get("image_fullscreen_001_landscape").get("u").asText();
-            if (imageUrl.lastIndexOf("?") != -1) {
-                imageId = imageUrl.substring(imageUrl.lastIndexOf("/") + 1, imageUrl.lastIndexOf("?"));
+            if (!ignoreRequest) {
+                item = OBJECT_MAPPER.readTree(body).get("batchrsp").get("items").get(0).get("item").asText();
+                JsonNode jsonNode = OBJECT_MAPPER.readTree(item);
+                imageUrl = jsonNode.get("ad").get("image_fullscreen_001_landscape").get("u").asText();
+                if (imageUrl.lastIndexOf("?") != -1) {
+                    imageId = imageUrl.substring(imageUrl.lastIndexOf("/") + 1, imageUrl.lastIndexOf("?"));
+                } else {
+                    imageId = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
+                }
+                jsonNode = jsonNode.get("ad").get("title_text");
+                if (jsonNode != null) {
+                    imageDescription = jsonNode.get("tx").asText();
+                } else {
+                    imageDescription = imageId;
+                }
             } else {
-                imageId = imageUrl.substring(imageUrl.lastIndexOf("/") + 1);
-            }
-            jsonNode = jsonNode.get("ad").get("title_text");
-            if (jsonNode != null) {
-                imageDescription = jsonNode.get("tx").asText();
-            } else {
-                imageDescription = imageId;
+                return null;
             }
         } catch (Exception e) {
             throw new Exception(String.format("Error while fetching image data (response: %s)", body), e);
